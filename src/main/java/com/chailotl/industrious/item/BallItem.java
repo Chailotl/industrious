e6f567bd9995cbf0901093918e54a5d261615a62
Chai <7232280+Chailotl@users.noreply.github.com>
2024-11-10 22:01:50 -0500

    Added dispenser behavior to the balls

--- src/main/java/com/chailotl/industrious/item/BallItem.java
+++ src/main/java/com/chailotl/industrious/item/BallItem.java
@@ -1,6 +1,8 @@
 package com.chailotl.industrious.item;
 
 import com.chailotl.industrious.entity.BallEntity;
+import net.minecraft.block.DispenserBlock;
+import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -9,6 +11,7 @@ import net.minecraft.sound.SoundEvents;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.math.*;
 import net.minecraft.world.World;
 
 public class BallItem extends Item
@@ -16,6 +19,35 @@ public class BallItem extends Item
 	public BallItem(Settings settings)
 	{
 		super(settings);
+
+		DispenserBlock.registerBehavior(this, new ItemDispenserBehavior() {
+			@Override
+			protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack)
+			{
+				World world = pointer.world();
+				Direction direction = pointer.state().get(DispenserBlock.FACING);
+				Position position = DispenserBlock.getOutputLocation(pointer, 0.7, new Vec3d(0.0, 0.1, 0.0));
+				BallEntity ballEntity = new BallEntity(world, position.getX(), position.getY(), position.getZ());
+				ballEntity.setStack(stack);
+				ballEntity.setVelocity(
+					direction.getOffsetX(),
+					direction.getOffsetY(),
+					direction.getOffsetZ(),
+					1f,
+					1f
+				);
+
+				world.spawnEntity(ballEntity);
+				stack.decrement(1);
+				return stack;
+			}
+
+			@Override
+			protected void playSound(BlockPointer pointer)
+			{
+				pointer.world().syncWorldEvent(1002, pointer.pos(), 0);
+			}
+		});
 	}
 
 	@Override
@@ -36,10 +68,10 @@ public class BallItem extends Item
 
 		if (!world.isClient)
 		{
-			BallEntity ball = new BallEntity(world, user);
-			ball.setStack(stack);
-			ball.setVelocity(user, user.getPitch(), user.getYaw(), -20, 1f, 1f);
-			world.spawnEntity(ball);
+			BallEntity ballEntity = new BallEntity(world, user);
+			ballEntity.setStack(stack);
+			ballEntity.setVelocity(user, user.getPitch(), user.getYaw(), -20, 1f, 1f);
+			world.spawnEntity(ballEntity);
 		}
 
 		user.incrementStat(Stats.USED.getOrCreateStat(this));
